85f77eebc353162cfc3baa8ced860e4d0986a0c9
Dominik Häußer <haeusser-dominik@web.de>
2023-08-29 14:49:06 +0200

    this fixes #1

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Picture(models.Model):
+    name = models.CharField(max_length=30, default="unnamed")
+    image = models.ImageField(null=True, blank=False, upload_to="media")
+    description = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    TYPES = [
+        ("TR", "Truth"),
+        ("MU", "Multi"),
+        ("SI", "Single"),
+        ("TE", "Text")
+    ]
+    TOPICS = [
+        ("GE", "Geography"),
+        ("EN", "Entertainment"),
+        ("HI", "History"),
+        ("AL", "Art and Literature"),
+        ("ST", "Science and Technology"),
+        ("SP", "Sports and Pleasure")
+    ]
+    question_text = models.CharField(max_length=100, null=True, blank=False, unique=True)
+    question_type = models.CharField(max_length=2, choices=TYPES, null=True, blank=False)
+    reference_link = models.URLField(max_length=200, null=True, blank=True)
+    topic = models.CharField(max_length=2, choices=TOPICS, null=True, blank=False)
+    picture = models.ForeignKey(Picture, on_delete=models.CASCADE, null=True, blank=True)
+    already_asked = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return f'{self.question_type} - {self.question_text}'
+
+
+class TruthAnswer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=False)
+    is_true = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return f'{self.question.question_text} - {self.is_true}'
+
+
+class BasicAnswer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=False)
+    answer_text = models.CharField(max_length=60, null=True, blank=False)
+    is_true = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return f'{self.question.question_text} - {self.answer_text} - {self.is_true}'
+
+
+class Round(models.Model):
+    MODIS = [
+        ("M", "MIXED"),
+        ("T", "TOPIC")
+    ]
+    modus = models.CharField(max_length=1, choices=MODIS, null=True, blank=False)
+    questions = models.ManyToManyField(Question)
+    user = models.ForeignKey(User, null=True, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} - {self.modus}'
+
+
+class Statistics(models.Model):
+    user = models.ForeignKey(User, null=True, blank=False, on_delete=models.CASCADE)
+    answered_questions_count = models.IntegerField(default=0)
+    right_answers_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id} - {self.user.username}'
+
+
+admin.site.register(Picture)
+admin.site.register(Question)
+admin.site.register(TruthAnswer)
+admin.site.register(BasicAnswer)
+admin.site.register(Round)
+admin.site.register(Statistics)
--- quizWebApp/settings.py
+++ quizWebApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'questions.apps.QuestionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -121,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Files
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
